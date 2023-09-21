e1ecfc5f410fc387cb46f9dff3fb0d4257102ac1
Ajay Tanwar <87845392+Ajtanwar95@users.noreply.github.com>
2023-09-21 21:21:27 +0530

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
           </div>
         </Link>
 
-        <nav className="gap-4 md:gap-6 text-base md:text-lg hidden md:flex ">
+        <nav className="gap-4 mr-[-519px] md:gap-6 text-base md:text-lg hidden md:flex ">
           <Link
             className="hover:text-slate-100 transition duration-300 hover:rounded-md"
             to={""}
@@ -63,21 +63,17 @@ const Header = () => {
             Contact
           </Link>
         </nav>
-      </div>
 
-      {/* Mobile Header */}
-      <div className="md:hidden flex items-center justify-between">
-        <Link to={""}>
-          <div className="h-[70px] w-[200px]">
-            <img
-              src={logo}
-              className="h-full animate-bounce w-full"
-              alt="Logo"
-            />
-          </div>
-        </Link>
-
+        {/* User Profile and Cart Icons */}
         <div className="flex items-center gap-4">
+          <Link to={"cart"}>
+            <div className="text-2xl text-slate-600 relative animate-bounce">
+              <BsCartFill />
+              <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 p-0 text-sm text-center">
+                {cartItemNumber.length}
+              </div>
+            </div>
+          </Link>
           <div className=" text-slate-600" onClick={handleShowMenu}>
             <div className="text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md">
               {userData.image ? (
@@ -106,7 +102,7 @@ const Header = () => {
                     className="cursor-pointer text-white px-2 bg-red-500"
                     onClick={handleLogout}
                   >
-                    Logout ({userData.firstName}){" "}
+                    Logout ({userData.firstName})
                   </p>
                 ) : (
                   <Link
@@ -136,7 +132,23 @@ const Header = () => {
               </div>
             )}
           </div>
+        </div>
+      </div>
 
+      {/* Mobile Header */}
+      <div className="md:hidden flex items-center justify-between">
+        <Link to={""}>
+          <div className="h-[70px] w-[200px]">
+            <img
+              src={logo}
+              className="h-full animate-bounce w-full"
+              alt="Logo"
+            />
+          </div>
+        </Link>
+
+        {/* User Profile and Cart Icons */}
+        <div className="flex items-center gap-4">
           <Link to={"cart"}>
             <div className="text-2xl text-slate-600 relative animate-bounce">
               <BsCartFill />
@@ -145,6 +157,64 @@ const Header = () => {
               </div>
             </div>
           </Link>
+          <div className=" text-slate-600" onClick={handleShowMenu}>
+            <div className="text-3xl cursor-pointer w-8 h-8 rounded-full overflow-hidden drop-shadow-md">
+              {userData.image ? (
+                <img
+                  src={userData.image}
+                  className="h-full w-full"
+                  alt="User"
+                />
+              ) : (
+                <HiOutlineUserCircle />
+              )}
+            </div>
+            {showMenu && (
+              <div className="absolute top-16 right-0 bg-white py-2 shadow drop-shadow-md flex flex-col min-w-[120px] text-center">
+                {userData.email === process.env.REACT_APP_ADMIN_EMAIL && (
+                  <Link
+                    to={"newproduct"}
+                    className="whitespace-nowrap cursor-pointer px-2"
+                  >
+                    New product
+                  </Link>
+                )}
+
+                {userData.image ? (
+                  <p
+                    className="cursor-pointer text-white px-2 bg-red-500"
+                    onClick={handleLogout}
+                  >
+                    Logout ({userData.firstName})
+                  </p>
+                ) : (
+                  <Link
+                    to={"login"}
+                    className="whitespace-nowrap cursor-pointer px-2"
+                  >
+                    Login
+                  </Link>
+                )}
+                <nav className="text-base md:text-lg flex flex-col md:hidden">
+                  <Link to={""} className="px-2 py-1">
+                    Home
+                  </Link>
+                  <Link
+                    to={"menu/63f0fdbb3bcc2f97fa53d25d"}
+                    className="px-2 py-1"
+                  >
+                    Menu
+                  </Link>
+                  <Link to={"about"} className="px-2 py-1">
+                    About
+                  </Link>
+                  <Link to={"contact"} className="px-2 py-1">
+                    Contact
+                  </Link>
+                </nav>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </header>
